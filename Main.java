3428c7d59e4ec79db798361d58575dd5a6d242a8
Jayden Wynes <jayden.wynes@gmail.com>
2022-02-11 20:30:49 +0000

    Created main menu, encountering issue with Inventory Object

--- Main.java
+++ Main.java
@@ -13,13 +13,31 @@ public class Main {
 			System.out.println("2. Add a random Item");
 			System.out.println("3. Drop an item");
 			System.out.println("4. Equip a weapon");
-			System.out.println("5. Equip some armor");
+			System.out.println("5. Equip Armor");
 			System.out.println("6. Quit");
+			System.out.print(": ");
+			choice = scan.nextInt();
 
+			if (choice == 1) {
+				Inventory.print(inventory);
+			} else if (choice == 2) {
+				inventory.add(ItemGenerator.generate());
+			} else if (choice == 3) {
+				System.out.println("Drop an item");
+
+			} else if (choice == 4) {
+				System.out.println("Equip a Weapon");
+
+			} else if (choice == 5) {
+				System.out.println("Equip Armor");
+			} else if (choice == 6) {
+				System.out.println("Now exiting your inventory system");
+				break;
+			} else {
+				System.out.println("Error: Invalid input, please try again");
+				choice = 0;
+			}		
 		
-		inventory.add(ItemGenerator.generate());
-		Inventory.print(inventory);
-		
-	}
+		}
+	}	
 }
-
