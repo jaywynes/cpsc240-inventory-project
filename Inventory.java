e13acb83f31f48a3d07b8cae8276c4e3b46d04a6
Jayden Wynes <jayden.wynes@gmail.com>
2022-02-11 06:19:56 +0000

    Created basis of initial menu, began drop method.

--- Inventory.java
+++ Inventory.java
@@ -1,11 +1,13 @@
+import java.util.Scanner;
+
 import java.util.ArrayList;
 
 public class Inventory {
-	ArrayList<Item> inventory = new ArrayList<>();
+	static ArrayList<Item> inventory = new ArrayList<Item>();
 	public int maxWeight;
 	private Item equippedWeapon;
 	private Item equippedArmor;
-
+	Scanner scan = new Scanner (System.in);
 	public Inventory() {
 		maxWeight = 200;
 		equippedWeapon = null;
@@ -31,12 +33,21 @@ public class Inventory {
 		return currentWeight;
 	}
 
-	public void print(Inventory i) {
+	public static void print(Inventory i) {
 		int invenCount = 1;
 		for (Item c : inventory) {
-			System.out.format("%d. %2s\t %10s %10d %10d %10d", invenCount, c.getName(), c.getType(), c.getWeight(), c.getValue(), c.getStrength());
+			System.out.format("   %4s\t %20s\t %5s\t %5s\t %5s\t %n", "Name", "Type", "Weight", "Value", "Strength");
+			System.out.format("%d. %2s\t %5s\t %5d\t %5d\t %5d\t %n", invenCount, c.getName(), c.getType(), c.getWeight(), c.getValue(), c.getStrength());
 			invenCount++;
 		}	
 	}
 
+	public static void drop(Inventory i) {
+		System.out.println("Please enter the number of which item you ould like to drop");
+		Inventory.print(inventory);
+		int itemToDrop = scan.nextInt() + 1;
+		
+	
+	}
+	
 }
--- Main.java
+++ Main.java
@@ -1,9 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
 		Inventory inventory = new Inventory();
+		int choice = 0;
+
+		while (choice != 6) {
+			System.out.println("----------------------------------------------------------------");
+			System.out.println("1. View your Inventory");
+			System.out.println("2. Add a random Item");
+			System.out.println("3. Drop an item");
+			System.out.println("4. Equip a weapon");
+			System.out.println("5. Equip some armor");
+			System.out.println("6. Quit");
+
+		
 		inventory.add(ItemGenerator.generate());
-		print(inventory);
+		Inventory.print(inventory);
+		
 	}
 }
 
